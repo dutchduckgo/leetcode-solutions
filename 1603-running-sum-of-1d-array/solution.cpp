a145307b05deaac2d34474cea56ac52893ad0aaf
Richard Shi <43119605+dutchduckgo@users.noreply.github.com>
2023-06-10 23:38:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (100.00%)

---
+++ 1603-running-sum-of-1d-array/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int cur = nums[0];
+        vector<int> rslt;
+        rslt.push_back(cur);
+        for (int i = 1; i < nums.size(); i ++) {
+            rslt.push_back(cur + nums[i]);
+            cur += nums[i];
+        }
+        return rslt;
+    }
+};
