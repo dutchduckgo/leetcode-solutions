39d4b500121cb5487ffb66a93740b52ad3d5165b
Richard Shi <43119605+dutchduckgo@users.noreply.github.com>
2023-06-10 23:49:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (100.00%)

---
+++ 0412-fizz-buzz/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> rslt;
+
+        for (int i = 1; i <= n; i ++) {
+            if (i % 15 == 0) rslt.push_back("FizzBuzz");
+            else if (i % 3 == 0) rslt.push_back("Fizz");
+            else if (i % 5 == 0) rslt.push_back("Buzz");
+            else rslt.push_back(to_string(i));
+
+        }
+        return rslt;
+    }
+};
