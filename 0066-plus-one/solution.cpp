7690c4579589a860e47eab41855d6efb85db185e
Richard Shi <43119605+dutchduckgo@users.noreply.github.com>
2024-05-10 14:58:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.5 MB (100.00%)

---
+++ 0066-plus-one/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        if (digits[n- 1] != 9) {
+            digits[n - 1] ++;
+            return digits; 
+        }
+
+        for (int i = n - 1; i >= 0; i --) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+                continue;
+            }
+            digits[i] ++;
+            return digits;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
