2444f00c06ddfbe3b4f2ca29049120f3166ea02c
Richard Shi <43119605+dutchduckgo@users.noreply.github.com>
2025-03-04 04:32:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.4 MB (99.98%)

---
+++ 0226-invert-binary-tree/solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        help(root);
+        return root;
+    }
+    private void help(TreeNode node) {
+        if (node == null) return;
+        if (node.left == null && node.right == null) return;
+
+            TreeNode temp = node.left;
+            node.left = node.right;
+            node.right = temp;
+            help(node.left);
+            help(node.right);
+        
+
+    }
+}
