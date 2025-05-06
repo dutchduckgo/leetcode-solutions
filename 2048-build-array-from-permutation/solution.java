2b814d70c18931e1a1dbb59770cc8ea82f302c60
Richard Shi <43119605+dutchduckgo@users.noreply.github.com>
2025-05-06 00:17:42 +0000

    [LeetCode Sync] Runtime - 1 ms (99.48%), Memory - 45.1 MB (99.73%)

--- 2048-build-array-from-permutation/solution.java
+++ 2048-build-array-from-permutation/solution.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length];
-        for (int i = 0; i < nums.length; i ++) {
+        int len = nums.length;
+        int[] ans = new int[len];
+        for (int i = 0; i < len; i ++) {
             ans[i] = nums[nums[i]];
         }
         return ans;
