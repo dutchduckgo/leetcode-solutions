38d8979d499b60a2b9056906371689c041143f37
Richard Shi <43119605+dutchduckgo@users.noreply.github.com>
2025-08-01 05:29:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.7 MB (63.62%)

--- 0118-pascals-triangle/solution.cpp
+++ 0118-pascals-triangle/solution.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> triangle;
+        if (numRows == 0) return triangle;
 
-        if(numRows == 0) return triangle;
+        triangle.push_back( {1} );
 
-        vector<int> firstRow;
-        firstRow.push_back(1);
-        triangle.push_back(firstRow);
-
-        for(int i = 1; i < numRows; i ++) {
-            vector<int> prev = triangle[i - 1];
+        for (int i = 1; i < numRows; i ++) {
             vector<int> row;
-
             row.push_back(1);
+            
+            vector<int> prev = triangle[i - 1];
 
             for (int j = 1; j < i; j ++) {
-                row.push_back(prev[j -1] + prev[j]);
+                row.push_back(prev[j - 1] + prev[j]);
             }
-
             row.push_back(1);
             triangle.push_back(row);
         }
         return triangle;
 
-        
     }
 };
