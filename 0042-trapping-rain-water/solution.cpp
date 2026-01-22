ba70d3e0fee33cb26f8e45659b92404542ead395
Richard Shi <43119605+dutchduckgo@users.noreply.github.com>
2026-01-22 05:50:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 26 MB (78.56%)

---
+++ 0042-trapping-rain-water/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        int l = 0, r = height.size() - 1;
+        int maxL = height[l], maxR = height[r];
+
+        int res = 0;
+
+        while (l < r) {
+            if (maxL < maxR) {
+                l++;
+                maxL = max(maxL, height[l]);
+                res += abs(maxL - height[l]);
+            } else {
+                r--;
+                maxR = max(maxR, height[r]);
+                res += abs(maxR - height[r]);
+            }
+        }
+        return res;
+
+    }
+};
+
